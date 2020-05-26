a9e5884d9455aa20e151744abe1405d2cd9736cf
DimasGR64 <60886561+DimasGR64@users.noreply.github.com>
2020-05-26 22:14:10 +0400

    Update

--- Task_1.1/Task_1.2.3/Program.cs
+++ Task_1.1/Task_1.2.3/Program.cs
@@ -16,11 +16,11 @@ namespace Task_1._2._3
             string input = "Антон хорошо начал утро: посушал Стинга, выпил кофе и посмотрел Звездные Войны";
             string[] words = input.Split(new char[] { ' ' }) ;
             int lowerword = 0;
-            //string word;
+            string word;
 
             for (int i = 0; i < words.Length; i++)
             {
-                string word = words[i];
+                word = words[i];
                 if (char.IsLower(word[0]))
                 {
                     lowerword++;
