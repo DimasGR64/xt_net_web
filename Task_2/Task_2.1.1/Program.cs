f39ba2131bcf4cf6139b335646434a0d49c047dc
DimasGR64 <60886561+DimasGR64@users.noreply.github.com>
2020-06-28 21:53:30 +0400

    After feedback

--- Task_2/Task_2.1.1/Program.cs
+++ Task_2/Task_2.1.1/Program.cs
@@ -17,49 +17,4 @@ namespace Task_2._1._1
             char[] arrsymb = { 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd' };
         }
     }
-    class OperationOnString
-    {
-        public void Comparison(string str1, string str2) 
-        {
-            int value = String.CompareOrdinal(str1, str2);
-            if (value == 0)
-            {
-                Console.WriteLine("string equal");
-            }
-            else
-            {
-                Console.WriteLine("string not equal");
-            }
-        }
-        public static string Concat(string str1, string str2) 
-        {
-            return str1 + str2;
-        }
-        public bool FoundSimbol(char[] arrsymb, char symb) 
-        {
-            if (arrsymb.Contains(symb))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public static char[] StringToChar(string str) 
-        {
-            char[] symb;
-            return symb = str.ToCharArray(); ;
-        }
-        public static string CharTostring(char[] symb) 
-        {
-            string str = ("");
-            for (int i = 0; i < symb.Length; i++)
-            {
-                string buffer = symb[i].ToString();
-                str = str + buffer;
-            }
-            return str;
-        }
-    }
 }
