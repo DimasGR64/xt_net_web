8c76485ca5baef2c05b81c277adde687d2b87a4c
DimasGR64 <60886561+DimasGR64@users.noreply.github.com>
2020-08-31 00:17:47 +0400

    add

---
+++ Task_6/6.5/storage.js
@@ -0,0 +1,24 @@
+﻿﻿class Service {
+    constructor() {
+        this.arr = [];
+    }
+    add(newObj = null) {
+        if (newObj === null)
+            return false;
+        if (this.arr.includes(newObj))
+            return false;
+        newObj.id = ''.replace(/[xy]/g, (c, r) => ('x' == c ? (r = Math.random() * 16 | 0) : (r & 0x3 | 0x8)).toString(16));
+        this.arr.push(newObj);
+        return true;
+    }
+    deleteById(id = '') {
+        for (let i = 0; i < this.arr.length; i++) {
+            if (this.arr[i].id == id) {
+                let deletedObj = JSON.parse(JSON.stringify(this.arr[i]));
+                this.arr.splice(i, 1);
+                return deletedObj;
+            }
+        }
+        return null;
+    }
+}
